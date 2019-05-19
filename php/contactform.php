c02fca093fb1998db9ac1953faa0bdae2bfd38f9
Sergey <sergeybronov_github@mail.ru>
2019-05-19 18:19:35 +0300

    Commit

--- php/contactform.php
+++ php/contactform.php
@@ -1,11 +1,11 @@
 <?php
 
 if (isset($_POST['submit'])) {
-    $name= $_POST['name'];
-    $phone= $_POST['phone'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
 
-    $mailTo= "webworks7@mail.ru";
-    $txt= "You got message from ".$name.".\n\n".$phone;
+    $mailTo = "webworks7@mail.ru";
+    $txt = "You got message from ".$name.".\n\n".$phone;
 
     mail( $mailTo, $txt);
     header("Location: index.php?mailsend");
